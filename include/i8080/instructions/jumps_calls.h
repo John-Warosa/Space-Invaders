41153613ea687262721d417f117656963dc63bd8
John-Warosa <elias.mat100@gmail.com>
2025-03-11 16:01:06 +0100

    added some jump call declarations

--- include/i8080/instructions/jumps_calls.h
+++ include/i8080/instructions/jumps_calls.h
@@ -3,22 +3,49 @@
 
 #include "types/instruction.h"
 
+// Return if zero bit is not set
+// Takes up 1 Byte and 11/5 CPU cycles
+// Affects no flags
 instruction RNZ;
 
+// Return if carry bit is not set
+// Takes up 1 Byte and 11/5 CPU cycles
+// Affects no flags
 instruction RNC;
 
+// Return if parity bit is not set
+// Takes up 1 Byte and 11/5 CPU cycles
+// Affects no flags
 instruction RPO;
 
+// Return if sign bit is not set
+// Takes up 1 Byte and 11/5 CPU cycles
+// Affects no flags
 instruction RP;
 
+// jump to <ADDR> if zero bit is not set
+// Takes up 3 Bytes and 10 CPU cycles
+// Affects no flags
 instruction JNZ_ADDR;
 
+// Jump to <ADDR> if carry bit is not set
+// Takes up 3 Bytes and 10 CPU cycles
+// Affects no flags
 instruction JNC_ADDR;
 
+// Jump to <ADDR> if parity bit is not set
+// Takes up 3 Bytes and 10 CPU cycles
+// Affects no flags
 instruction JPO_ADDR;
 
+// Jump to <ADDR> if sign bit is not set
+// Takes up 3 Bytes and 10 CPU cycles
+// Affects no flags
 instruction JP_ADDR;
 
+// Jump to <ADDR>
+// Takes up 3 Bytes and 10 CPU cycles
+// Affects no flags
 instruction JMP_ADDR;
 
 #endif // JUMPS_CALLS_H
